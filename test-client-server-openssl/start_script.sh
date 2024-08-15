1157030f982c94c194237f6750c3568515d8cbb3
Ilija Obradovic <ikan2001@gmail.com>
2024-08-16 01:06:43 +0200

    Some more changes

--- test-client-server-openssl/start_script.sh
+++ test-client-server-openssl/start_script.sh
@@ -5,14 +5,15 @@ if [ $# != 1 ]; then
 	echo "$0 <s | c>"
 	echo "	s - server"
 	echo "	c - client"
+	exit
 fi
 
 PATH=$PATH:$(pwd)/bin/
 
-if [ $1 == 's']; then
+if [ "$1" == 's']; then
 	./ssl_echo s 7002 
 fi
 
-if [ $1 == 'c']; then
+if [ "$1" == 'c']; then
 	./ssl_echo c localhost 7020
 fi
